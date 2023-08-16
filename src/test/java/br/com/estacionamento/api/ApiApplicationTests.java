e9eca7bfa85a1c7d61673599ecc6e9f1ba544d33
ikedecrocs <mateuscaden@gmail.com>
2023-08-16 12:26:03 -0300

    Arquivos iniciais

---
+++ src/test/java/br/com/estacionamento/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estacionamento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
